45e6cbf270ba4e5d4bec0002626e9348ffc0573a
xingkyh <1726284922@qq.com>
2021-01-18 00:39:08 +0800

    添加收藏和浏览记录展示

--- app/src/main/java/com/example/maogai/ui/slideshow/SlideshowFragment.java
+++ app/src/main/java/com/example/maogai/ui/slideshow/SlideshowFragment.java
@@ -4,32 +4,67 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProviders;
+import androidx.fragment.app.FragmentTransaction;
+
 
 import com.example.maogai.R;
+import com.example.maogai.sqliteUtil.SqlPage;
+import com.example.maogai.sqliteUtil.SqlUtil;
+import com.example.maogai.ui.fragment.WebViewFragment;
+
+import java.util.List;
 
 public class SlideshowFragment extends Fragment {
 
-    private SlideshowViewModel slideshowViewModel;
+    View root;
+    List<SqlPage> sqlPages;//查询到的历史
+    int index;//点击的记录
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        slideshowViewModel =
-                ViewModelProviders.of(this).get(SlideshowViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_slideshow, container, false);
-        final TextView textView = root.findViewById(R.id.text_slideshow);
-        slideshowViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+        root = inflater.inflate(R.layout.fragment_slideshow, container, false);
+        init();
         return root;
+    }
+
+    private void init(){
+        SqlUtil sqlUtil = new SqlUtil(getActivity());
+        sqlPages = sqlUtil.getALLCollection();
+        LinearLayout linearLayout = (LinearLayout) root.findViewById(R.id.fragment_layout_collection);
+        for (int i = 0; i < sqlPages.size(); i++){
+            TextView textView = new TextView(getActivity());
+            SqlPage sp = sqlPages.get(i);
+            textView.setText(sp.getChapter() + "：" + sp.getName());
+            textView.setTextSize(25);
+            final int j = i;
+            textView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    index = j;
+                    showFragment();
+                }
+            });
+            linearLayout.addView(textView);
+        }
+    }
+
+    private void showFragment(){
+        Fragment fragment = new WebViewFragment();
+        Bundle bundle = new Bundle();
+        SqlPage sqlPage = sqlPages.get(index);
+        bundle.putSerializable("chapter", sqlPage.getChapter());
+        bundle.putSerializable("name", sqlPage.getName());
+        bundle.putSerializable("url", sqlPage.getUrl());
+        fragment.setArguments(bundle);
+        FragmentTransaction transaction = getActivity().getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.nav_host_fragment, fragment)
+                .addToBackStack(null)
+                .commit();
     }
 }
